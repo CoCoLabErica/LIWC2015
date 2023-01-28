546f41af0d947ed52ae9c5962ded8b1674c80fe0
CoCoLabErica <30304984+CoCoLabErica@users.noreply.github.com>
2023-01-28 21:40:52 +1100

    Update the example code to show how specific categories from the results can be shown in console; Explain why no empty folder on github

--- example.R
+++ example.R
@@ -4,14 +4,16 @@ source("liwc2015.R")
 # Note: Require LIWC dictionary file .dic to use the liwc2015()
 
 # The example folder has 1 file
-liwc2015("liwc.dic","example")
+results <- liwc2015("liwc.dic","example")
 # [1] "Results saved as R-LIWC2015 Results JonBenetRamsey_note 2023-01-28 152805.csv" 
+results$WC # 382
+results$number # 3.66
 
 # The InauguralTexts subfolder has 2 files
 liwc2015("liwc.dic","example/InauguralTexts")
 # [1] "Results saved as R-LIWC2015 Results 01.Washington.1 - 02.Washington.2 (2 files) 2023-01-28 152935.csv"
 
-# Empty folder
+# Empty folder (empty folder is not on github because pushing empty folder not allowed)
 liwc2015("liwc.dic","example/empty")
 # [1] "No .txt file found, please check the input folder: example/empty"
 
